9c1c9359378afe00d52b0d068f2060f9fdd85244
chrisdotgrubb <developer.grubb@gmail.com>
2023-07-12 15:18:09 -0400

    Add custom user model

--- tunelist/settings.py
+++ tunelist/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tune'
+    'tune',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -108,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'account.MyUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
