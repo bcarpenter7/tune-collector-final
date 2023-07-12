b5776168842bf55abd3bfc98ad93477dd1e88980
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-07-12 15:15:57 -0400

    basic crud almost

--- tune/models.py
+++ tune/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+class Tune(models.Model):
+    name = models.CharField(max_length=100)
+    key = models.CharField(max_length=4)
+    fiddler = models.CharField(max_length=100)
+    tuning = models.CharField(max_length=4)
+    state = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    stars = models.IntegerField()
+
+    
+    def __str__(self) -> str:
+        return f'{self.name} {self.id}'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'tune_id': self.id})
--- tune/urls.py
+++ tune/urls.py
@@ -6,4 +6,8 @@ from . import views
 
 urlpatterns = [
 path('', views.home, name='home'),
+path('about/', views.about, name='about'),
+path('tunes/', views.tunes_index, name='index'),
+path('tunes/<int:tune_id>/', views.tunes_detail, name='detail'),
+path('tunes/create/', views.TuneCreate.as_view(), name='tunes_create')
 ]
