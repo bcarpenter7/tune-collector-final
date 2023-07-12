278b0df50d962d8e486427a630b59eb89241c974
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-07-12 15:21:19 -0400

    FULL CRUD PLUS EXPERIMENTAL STUFF

--- tune/views.py
+++ tune/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Tune
 # Create your views here.
 
@@ -26,3 +26,42 @@ class TuneCreate(CreateView):
     model = Tune
     fields = '__all__'
 
+class TuneUpdate(UpdateView):
+    model = Tune
+    fields = '__all__'
+
+class TuneDelete(DeleteView):
+    model = Tune
+    success_url = '/tunes'
+
+
+
+# EXPERIMENTAL DOWN HERE
+
+def tunes_index_d(request):
+    tunes = Tune.objects.filter(key='D').order_by('name')
+    return render(request, 'tunes/index.html', {
+        'tunes': tunes,
+        'title': 'D'
+    })
+
+def tunes_index_a(request):
+    tunes = Tune.objects.filter(key='A').order_by('name')
+    return render(request, 'tunes/index.html', {
+        'tunes': tunes,
+        'title': 'A'
+    })
+
+def tunes_index_g(request):
+    tunes = Tune.objects.filter(key='G').order_by('name')
+    return render(request, 'tunes/index.html', {
+        'tunes': tunes,
+        'title': 'G'
+    })
+
+def tunes_index_c(request):
+    tunes = Tune.objects.filter(key='C').order_by('name')
+    return render(request, 'tunes/index.html', {
+        'tunes': tunes,
+        'title': 'C'
+    })
