6e5e73c7f26e6bb2568c173a62b8fdafdf9a1cc9
chrisdotgrubb <developer.grubb@gmail.com>
2023-07-12 15:46:28 -0400

    Add custom user-admin page and remove groups

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from account.models import MyUser
+
+
+class MyUserAdmin(UserAdmin):
+	list_display = ('username', 'email', 'created_at', 'updated_at', 'last_login', 'is_staff', 'is_superuser', 'is_active')
+	search_fields = ('username', 'email')
+	fieldsets = (
+		(None, {'fields': ('username', 'email', 'password')}),
+		('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+		('Timestamps', {'fields': ('created', 'updated', 'last_login')}),
+	)
+	readonly_fields = ('created_at', 'updated_at', 'last_login')
+	add_fieldsets = ((None, {'fields': ('username', 'email', 'password1', 'password2')}), ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}))
+	ordering = ('username',)
+
+
+admin.site.register(MyUser, MyUserAdmin)
+admin.site.unregister(Group)
