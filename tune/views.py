5f0736a676831751ae6b205fef57a732d83eb626
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-07-12 15:15:57 -0400

    basic crud almost

--- tune/views.py
+++ tune/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from .models import Tune
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html' )
+
+def tunes_index(request):
+    tunes = Tune.objects.order_by('name')
+    return render(request, 'tunes/index.html', {
+        'tunes': tunes,
+        'title': 'All'
+    })
+
+def tunes_detail(request, tune_id):
+    tune = Tune.objects.get(id=tune_id)
+    return render(request, 'tunes/detail.html', {
+    'tune': tune
+})
+
+class TuneCreate(CreateView):
+    model = Tune
+    fields = '__all__'
+
