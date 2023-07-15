9acc33c40f9d68714340f10563675287f6bb6f88
chrisdotgrubb <developer.grubb@gmail.com>
2023-07-14 23:17:52 -0400

    Add ability to see other user's and their tunes

--- account/views.py
+++ account/views.py
@@ -1,41 +1,74 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
+from tune.models import Tune
 from .forms import RegistrationForm
+from .models import MyUser
 
 
 def user_registration_view(request):
-	if request.user.is_authenticated:
-		return redirect('/')
-	
-	if request.method == 'POST':
-		form = RegistrationForm(request.POST)
-		if form.is_valid():
-			user = form.save(commit=False)
-			user.email = form.cleaned_data['email']
-			user.set_password(form.cleaned_data['password2'])
-			user.save()
-			messages.add_message(request, messages.SUCCESS, 'Account created, please log in')
-			return redirect(reverse_lazy('account:login'))
-	else:
-		form = RegistrationForm()
-		
-	context = {
-		'form': form
-	}
-	return render(request, 'account/registration/register.html', context)
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.email = form.cleaned_data['email']
+            user.set_password(form.cleaned_data['password2'])
+            user.save()
+            messages.add_message(request, messages.SUCCESS, 'Account created, please log in')
+            return redirect(reverse_lazy('account:login'))
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'account/registration/register.html', context)
 
 
 def user_deletion_view(request):
-	if not request.user.is_authenticated:
-		return redirect('/')
-	return render(request, 'account/delete.html')
-	
+    if not request.user.is_authenticated:
+        return redirect('/')
+    return render(request, 'account/delete.html')
+
 
 def user_deletion_confirm(request):
-	if request.user.is_authenticated:
-		request.user.is_active = False
-		request.user.save()
-		messages.add_message(request, messages.SUCCESS, 'Account deleted')
-		return redirect('/')
+    if request.user.is_authenticated:
+        request.user.is_active = False
+        request.user.save()
+        messages.add_message(request, messages.SUCCESS, 'Account deleted')
+        return redirect('/')
+
+
+@login_required
+def index_view(request):
+    # get all active users other than currently logged-in
+    users = MyUser.objects.filter(~Q(pk=request.user.pk), is_active=True)
+    context = {
+        'users': users,
+    }
+    return render(request, 'account/index.html', context)
+
+
+@login_required
+def detail_view(request, pk, char):
+    user = MyUser.objects.get(pk=pk)
+    tunes = Tune.objects.filter(user=user).order_by('name')
+    distinct_keys = tunes.values_list('key').order_by('key').distinct()
+    avail_keys = [key[0] for key in distinct_keys]
+
+    if len(char) == 1:
+        tunes = tunes.filter(key__iexact=char)
+
+    context = {
+        'user': user,
+        'tunes': tunes,
+        'title': char.upper(),
+        'avail_keys': avail_keys,
+    }
+    return render(request, 'account/detail.html', context)
